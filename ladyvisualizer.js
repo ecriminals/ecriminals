b245723bacc21b252da0be26e9c0227c48a6783c
Lady <angelsec@reborn.com>
2024-12-19 01:21:04 -0500

    Update ladyvisualizer.js

--- ladyvisualizer.js
+++ ladyvisualizer.js
@@ -5,43 +5,38 @@ window.onload = function () {
     const ctx = canvas.getContext('2d');
     const audio = document.getElementById('audio');
 
-    
+
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    
     const audioContext = new (window.AudioContext || window.webkitAudioContext)();
     const analyser = audioContext.createAnalyser();
-    analyser.fftSize = 256;  // Number of frequency bins
+    analyser.fftSize = 256;  
 
-    
     const source = audioContext.createMediaElementSource(audio);
     source.connect(analyser);
     analyser.connect(audioContext.destination);
 
-    
     const bufferLength = analyser.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
 
     function renderFrame() {
         analyser.getByteFrequencyData(dataArray);
 
-        
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = 'rgba(0, 0, 0, 0.05)'; 
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-        
         const barWidth = (canvas.width / bufferLength) * 2.5;
         let barHeight;
         let x = 0;
 
+        
+        const color = 'rgba(0, 255, 255, 0.7)';  
+
         for (let i = 0; i < bufferLength; i++) {
             barHeight = dataArray[i];
 
-            const r = barHeight + 50 * (i / bufferLength);
-            const g = 250 * (i / bufferLength);
-            const b = 50;
-
-            ctx.fillStyle = `rgb(${r},${g},${b})`;
+            ctx.fillStyle = color;
             ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
 
             x += barWidth + 1;
@@ -51,7 +46,6 @@ window.onload = function () {
         requestAnimationFrame(renderFrame);
     }
 
-    
     audio.onplay = () => {
         audioContext.resume().then(() => {
             renderFrame();
